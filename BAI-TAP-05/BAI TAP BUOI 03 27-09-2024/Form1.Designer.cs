25d8ba633d3f1ffe99ba66b37bd70ced87049eff
Nguyen Truong Giang <giangliken@gmail.com>
2024-09-27 11:23:45 +0700

    Bai tap buoi 3 27-09-2024

---
+++ BAI-TAP-05/BAI TAP BUOI 03 27-09-2024/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace BAI_TAP_BUOI_03_27_09_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFullname = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblGioiTinh = new System.Windows.Forms.Label();
+            this.lblSoThich = new System.Windows.Forms.Label();
+            this.btnXuatThongTin = new System.Windows.Forms.Button();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnNam = new System.Windows.Forms.RadioButton();
+            this.btnNu = new System.Windows.Forms.RadioButton();
+            this.btnTheThao = new System.Windows.Forms.CheckBox();
+            this.btnPhimAnh = new System.Windows.Forms.CheckBox();
+            this.btnDuLich = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(48, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông tin";
+            // 
+            // lblFullname
+            // 
+            this.lblFullname.AutoSize = true;
+            this.lblFullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblFullname.Location = new System.Drawing.Point(57, 129);
+            this.lblFullname.Name = "lblFullname";
+            this.lblFullname.Size = new System.Drawing.Size(83, 29);
+            this.lblFullname.TabIndex = 1;
+            this.lblFullname.Text = "Họ tên";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblDate.Location = new System.Drawing.Point(567, 132);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(119, 29);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Ngày sinh";
+            // 
+            // lblGioiTinh
+            // 
+            this.lblGioiTinh.AutoSize = true;
+            this.lblGioiTinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblGioiTinh.Location = new System.Drawing.Point(57, 223);
+            this.lblGioiTinh.Name = "lblGioiTinh";
+            this.lblGioiTinh.Size = new System.Drawing.Size(101, 29);
+            this.lblGioiTinh.TabIndex = 3;
+            this.lblGioiTinh.Text = "Giới tính";
+            // 
+            // lblSoThich
+            // 
+            this.lblSoThich.AutoSize = true;
+            this.lblSoThich.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblSoThich.Location = new System.Drawing.Point(567, 223);
+            this.lblSoThich.Name = "lblSoThich";
+            this.lblSoThich.Size = new System.Drawing.Size(99, 29);
+            this.lblSoThich.TabIndex = 4;
+            this.lblSoThich.Text = "Sở thích";
+            // 
+            // btnXuatThongTin
+            // 
+            this.btnXuatThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnXuatThongTin.Location = new System.Drawing.Point(841, 414);
+            this.btnXuatThongTin.Name = "btnXuatThongTin";
+            this.btnXuatThongTin.Size = new System.Drawing.Size(208, 38);
+            this.btnXuatThongTin.TabIndex = 5;
+            this.btnXuatThongTin.Text = "Xuất thông tin";
+            this.btnXuatThongTin.UseVisualStyleBackColor = true;
+            this.btnXuatThongTin.Click += new System.EventHandler(this.btnXuatThongTin_Click);
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtFullName.Location = new System.Drawing.Point(168, 124);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(367, 34);
+            this.txtFullName.TabIndex = 6;
+            this.txtFullName.TextChanged += new System.EventHandler(this.txtFullName_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(716, 127);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(314, 34);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnNam
+            // 
+            this.btnNam.AutoSize = true;
+            this.btnNam.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnNam.Location = new System.Drawing.Point(203, 219);
+            this.btnNam.Name = "btnNam";
+            this.btnNam.Size = new System.Drawing.Size(85, 33);
+            this.btnNam.TabIndex = 8;
+            this.btnNam.TabStop = true;
+            this.btnNam.Text = "Nam";
+            this.btnNam.UseVisualStyleBackColor = true;
+            this.btnNam.CheckedChanged += new System.EventHandler(this.btnNam_CheckedChanged);
+            // 
+            // btnNu
+            // 
+            this.btnNu.AutoSize = true;
+            this.btnNu.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnNu.Location = new System.Drawing.Point(344, 219);
+            this.btnNu.Name = "btnNu";
+            this.btnNu.Size = new System.Drawing.Size(65, 33);
+            this.btnNu.TabIndex = 9;
+            this.btnNu.TabStop = true;
+            this.btnNu.Text = "Nữ";
+            this.btnNu.UseVisualStyleBackColor = true;
+            this.btnNu.CheckedChanged += new System.EventHandler(this.btnNu_CheckedChanged);
+            // 
+            // btnTheThao
+            // 
+            this.btnTheThao.AutoSize = true;
+            this.btnTheThao.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnTheThao.Location = new System.Drawing.Point(703, 219);
+            this.btnTheThao.Name = "btnTheThao";
+            this.btnTheThao.Size = new System.Drawing.Size(130, 33);
+            this.btnTheThao.TabIndex = 10;
+            this.btnTheThao.Text = "Thể thao";
+            this.btnTheThao.UseVisualStyleBackColor = true;
+            this.btnTheThao.CheckedChanged += new System.EventHandler(this.btnTheThao_CheckedChanged);
+            // 
+            // btnPhimAnh
+            // 
+            this.btnPhimAnh.AutoSize = true;
+            this.btnPhimAnh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnPhimAnh.Location = new System.Drawing.Point(841, 219);
+            this.btnPhimAnh.Name = "btnPhimAnh";
+            this.btnPhimAnh.Size = new System.Drawing.Size(135, 33);
+            this.btnPhimAnh.TabIndex = 11;
+            this.btnPhimAnh.Text = "Phim ảnh";
+            this.btnPhimAnh.UseVisualStyleBackColor = true;
+            this.btnPhimAnh.CheckedChanged += new System.EventHandler(this.btnPhimAnh_CheckedChanged);
+            // 
+            // btnDuLich
+            // 
+            this.btnDuLich.AutoSize = true;
+            this.btnDuLich.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDuLich.Location = new System.Drawing.Point(998, 219);
+            this.btnDuLich.Name = "btnDuLich";
+            this.btnDuLich.Size = new System.Drawing.Size(108, 33);
+            this.btnDuLich.TabIndex = 12;
+            this.btnDuLich.Text = "Du lịch";
+            this.btnDuLich.UseVisualStyleBackColor = true;
+            this.btnDuLich.CheckedChanged += new System.EventHandler(this.btnDuLich_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 669);
+            this.Controls.Add(this.btnDuLich);
+            this.Controls.Add(this.btnPhimAnh);
+            this.Controls.Add(this.btnTheThao);
+            this.Controls.Add(this.btnNu);
+            this.Controls.Add(this.btnNam);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.btnXuatThongTin);
+            this.Controls.Add(this.lblSoThich);
+            this.Controls.Add(this.lblGioiTinh);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblFullname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFullname;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblGioiTinh;
+        private System.Windows.Forms.Label lblSoThich;
+        private System.Windows.Forms.Button btnXuatThongTin;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton btnNam;
+        private System.Windows.Forms.RadioButton btnNu;
+        private System.Windows.Forms.CheckBox btnTheThao;
+        private System.Windows.Forms.CheckBox btnPhimAnh;
+        private System.Windows.Forms.CheckBox btnDuLich;
+    }
+}
+
